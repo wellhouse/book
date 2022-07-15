655dac8641a392ab07de5f9e28c7e3125b60cf19
wellsoares <well.soares.nunes@gmail.com>
2022-07-15 20:50:48 -0300

    swagger
    Implemenenting swagger

---
+++ src/test/java/com/recipebook/config/RecipeUnitTests.java
@@ -0,0 +1,28 @@
+//package com.recipebook.config;
+//
+//import com.recipebook.recipe.repository.RecipeRepository;
+//import com.recipebook.recipe.service.RecipeService;
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.extension.ExtendWith;
+//import org.mockito.Mock;
+//import org.mockito.junit.jupiter.MockitoExtension;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit.jupiter.SpringExtension;
+//
+//@ExtendWith(SpringExtension.class)
+//@SpringBootTest
+//@ExtendWith(MockitoExtension.class)
+//public class RecipeUnitTests {
+//
+//
+//    @Mock
+//    private RecipeRepository userRepository;
+//
+//    @Test
+//    void savedUserHasRegistrationDate() {
+//
+//
+//
+//    }
+//
+//}
